73777c505edcab084119e02a36e8d01ea39fd2e0
auauauu <leework@163.com>
2017-11-07 11:09:52 +0800

    add project
    add project

---
+++ src/main/java/com/guanglee/weixinSoft/WeixinSoftApplication.java
@@ -0,0 +1,12 @@
+package com.guanglee.weixinSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinSoftApplication.class, args);
+	}
+}
